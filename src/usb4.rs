intel/tbtools

commit 1f424f26ec11455280475e2dce50eb2875017599

margining: Use util::{RegBit, RegField} for HW results and SW errors parsing
Replace manual masks and shifts with RegBit and RegField definitions for
more concise code.

Signed-off-by: Aapo Vienamo <aapo.vienamo@linux.intel.com>
Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

src/usb4.rs
@@ -94,14 +94,25 @@ pub mod margin {
         pub type TimeSteps = RegField<15, 11>;
         pub type TimeOffset = RegField<20, 16>;
     }
+
+    pub mod hw_res_1 {
+        use crate::util;
+        type RegBit<const BIT: u32> = util::RegBit<1, BIT>;
+        type RegField<const LOW: u32, const HIGH: u32> = util::RegField<1, LOW, HIGH>;
+        pub type HighRightMarginRX0 = RegField<6, 0>;
+        pub type HighRightExceedsRX0 = RegBit<7>;
+        pub type LowLeftMarginRX0 = RegField<14, 8>;
+        pub type LowLeftExceedsRX0 = RegBit<15>;
+        pub type HighRightMarginRX1 = RegField<22, 16>;
+        pub type HighRightExceedsRX1 = RegBit<23>;
+        pub type LowLeftMarginRX1 = RegField<30, 24>;
+        pub type LowLeftExceedsRX1 = RegBit<31>;
+    }
+
+    pub mod sw_err {
+        use crate::util;
+        type RegField<const LOW: u32, const HIGH: u32> = util::RegField<0, LOW, HIGH>;
+        pub type RX0 = RegField<3, 0>;
+        pub type RX1 = RegField<7, 4>;
+    }
 }
-
-pub const MARGIN_HW_RES_1_MARGIN_MASK: u32 = genmask!(6, 0);
-pub const MARGIN_HW_RES_1_EXCEEDS: u32 = 1 << 7;
-pub const MARGIN_HW_RES_1_RX0_LL_MARGIN_SHIFT: u32 = 8;
-pub const MARGIN_HW_RES_1_RX1_RH_MARGIN_SHIFT: u32 = 16;
-pub const MARGIN_HW_RES_1_RX1_LL_MARGIN_SHIFT: u32 = 24;
-
-pub const MARGIN_SW_ERR_RX0_MASK: u32 = genmask!(3, 0);
-pub const MARGIN_SW_ERR_RX1_MASK: u32 = genmask!(7, 4);
-pub const MARGIN_SW_ERR_RX1_SHIFT: u32 = 4;

src/margining.rs
@@ -391,11 +391,12 @@ impl Results {
     /// Depending on which lane was selected returns tuple of values in either `mV` or `UI` for
     /// each margin.
     pub fn high_right_margin(&self, lane: &Lanes) -> (f64, f64) {
-        let lane0_margin = self.to_margin(self.result[1] & usb4::MARGIN_HW_RES_1_MARGIN_MASK);
-        let lane1_margin = self.to_margin(
-            (self.result[1] >> usb4::MARGIN_HW_RES_1_RX1_RH_MARGIN_SHIFT)
-                & usb4::MARGIN_HW_RES_1_MARGIN_MASK,
-        );
+        let lane0_margin = self.to_margin(usb4::margin::hw_res_1::HighRightMarginRX0::get_field(
+            &self.result,
+        ));
+        let lane1_margin = self.to_margin(usb4::margin::hw_res_1::HighRightMarginRX1::get_field(
+            &self.result,
+        ));
 
         match *lane {
             Lanes::Lane0 => (lane0_margin, 0.0),
@@ -406,11 +407,8 @@ impl Results {
 
     /// Returns `true` if high (or right) margin exceeds the maximum offset.
     pub fn high_right_margin_exceeds(&self, lane: &Lanes) -> (bool, bool) {
-        let lane0_exceeds =
-            (self.result[1] & usb4::MARGIN_HW_RES_1_EXCEEDS) == usb4::MARGIN_HW_RES_1_EXCEEDS;
-        let lane1_exceeds = ((self.result[1] >> usb4::MARGIN_HW_RES_1_RX1_RH_MARGIN_SHIFT)
-            & usb4::MARGIN_HW_RES_1_EXCEEDS)
-            == usb4::MARGIN_HW_RES_1_EXCEEDS;
+        let lane0_exceeds = usb4::margin::hw_res_1::HighRightExceedsRX0::get_bit(&self.result);
+        let lane1_exceeds = usb4::margin::hw_res_1::HighRightExceedsRX1::get_bit(&self.result);
 
         match *lane {
             Lanes::Lane0 => (lane0_exceeds, false),
@@ -421,14 +419,13 @@ impl Results {
 
     /// Returns low (or left) margin values in `mV` or `UI`.
     pub fn low_left_margin(&self, lane: &Lanes) -> (f64, f64) {
-        let lane0_margin = self.to_margin(
-            (self.result[1] >> usb4::MARGIN_HW_RES_1_RX0_LL_MARGIN_SHIFT)
-                & usb4::MARGIN_HW_RES_1_MARGIN_MASK,
-        );
-        let lane1_margin = self.to_margin(
-            (self.result[1] >> usb4::MARGIN_HW_RES_1_RX1_LL_MARGIN_SHIFT)
-                & usb4::MARGIN_HW_RES_1_MARGIN_MASK,
-        );
+        let lane0_margin = self.to_margin(usb4::margin::hw_res_1::LowLeftMarginRX0::get_field(
+            &self.result,
+        ));
+        let lane1_margin = self.to_margin(usb4::margin::hw_res_1::LowLeftMarginRX1::get_field(
+            &self.result,
+        ));
+
         match *lane {
             Lanes::Lane0 => (lane0_margin, 0.0),
             Lanes::Lane1 => (lane1_margin, 0.0),
@@ -438,12 +435,8 @@ impl Results {
 
     /// Returns `true` if low (or left) margin exceeds the maximum offset.
     pub fn low_left_margin_exceeds(&self, lane: &Lanes) -> (bool, bool) {
-        let lane0_exceeds = ((self.result[1] >> usb4::MARGIN_HW_RES_1_RX0_LL_MARGIN_SHIFT)
-            & usb4::MARGIN_HW_RES_1_EXCEEDS)
-            == usb4::MARGIN_HW_RES_1_EXCEEDS;
-        let lane1_exceeds = ((self.result[1] >> usb4::MARGIN_HW_RES_1_RX1_LL_MARGIN_SHIFT)
-            & usb4::MARGIN_HW_RES_1_EXCEEDS)
-            == usb4::MARGIN_HW_RES_1_EXCEEDS;
+        let lane0_exceeds = usb4::margin::hw_res_1::LowLeftExceedsRX0::get_bit(&self.result);
+        let lane1_exceeds = usb4::margin::hw_res_1::LowLeftExceedsRX1::get_bit(&self.result);
         match *lane {
             Lanes::Lane0 => (lane0_exceeds, false),
             Lanes::Lane1 => (lane1_exceeds, false),
@@ -453,9 +446,8 @@ impl Results {
 
     /// Returns error counters used with software margining.
     pub fn error_counter(&self, lane: &Lanes) -> (u32, u32) {
-        let lane0_counter = self.result[1] & usb4::MARGIN_SW_ERR_RX0_MASK;
-        let lane1_counter =
-            (self.result[0] & usb4::MARGIN_SW_ERR_RX1_MASK) >> usb4::MARGIN_SW_ERR_RX1_SHIFT;
+        let lane0_counter = usb4::margin::sw_err::RX0::get_field(&self.result);
+        let lane1_counter = usb4::margin::sw_err::RX1::get_field(&self.result);
 
         match *lane {
             Lanes::Lane0 => (lane0_counter, 0u32),

commit 0fda9e184c113bdcd37ad6322485f5b284cd7de3

usb4: Use correct double word for software margining error counters
Error counters start from the second double words. Fix this again.

Fixes: 1f424f2 ("margining: Use util::{RegBit, RegField} for HW results and SW errors parsing")
Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

src/usb4.rs
@@ -132,7 +132,7 @@ pub mod margin {
 
     pub mod sw_err {
         use crate::util;
-        type RegField<const LOW: u32, const HIGH: u32> = util::RegField<0, LOW, HIGH>;
+        type RegField<const LOW: u32, const HIGH: u32> = util::RegField<1, LOW, HIGH>;
         pub type RX0 = RegField<3, 0>;
         pub type RX1 = RegField<7, 4>;
     }